9056e97bda2796e758ce7af86bb26d3d6dbdadb9
Keilyn Ramirez <keilyn148@gmail.com>
2021-03-05 12:39:39 -0600

    working in achievements

--- view/achievementsView.php
+++ view/achievementsView.php
@@ -4,19 +4,42 @@ include_once 'public/headerSections.php';
 ?>
 <div class="container">
     <h1 class="m-4 text-center titulosProductos2">Logros de la empresa</h1>
-</div>
 
-<div class="container">
-    <h1 class="m-4 text-center titulosProductos2">Logros de la empresa</h1>
-</div>
-<div class="container">
-    <h1 class="m-4 text-center titulosProductos2">Logros de la empresa</h1>
-</div>
-<div class="container">
-    <h1 class="m-4 text-center titulosProductos2">Logros de la empresa</h1>
-</div>
+    <div class="row text-center align-items-center">
+        <div class="col-6 text-center ">
+            <img src="public/img/Producto1.jpg" alt="Queso Fresco" width="400" height=400">
+        </div>
+        <div class="col-6">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        </div>
+    </div>
+
+    <div class="row text-center align-items-center">
+        <div class="col-6 text-center ">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        </div>
+        <div class="col-6">
+
+            <img src="public/img/Producto1.jpg" alt="Queso Fresco" width="400" height=400">
+        </div>
+    </div>
 
 
+</div>
 
 
 
