7653a029b269e31c4ffee7babf2c79b188140b75
Emily Melendez <emi.mci.95@gmail.com>
2021-03-09 14:38:01 -0600

    .

--- public/header.php
+++ public/header.php
@@ -24,74 +24,69 @@
             <img class="img-fluid" alt="Productos lacteos la Herencia" src="public/img/logoHerencia.png" width="300"
                  height="150"/>
             <header>
-
-                <div class="topnav" id="myTopnav">
-                    <a href="?controlador=Index&accion=mostrar" class="active">Inicio</a>
-                    <a href="?controlador=History&accion=showHistory" class="active">Nuestra Historia</a>
-                    <a href="?controlador=Products&accion=showProducts" class="active">Productos</a>
-                    <a href="?controlador=Achievements&accion=showAchievements" class="active">Logros</a>
-                    <a href="?controlador=Contact&accion=showContact" class="active">Contactos</a>
-                    <a href="?controlador=VisitUs&accion=showVisitUs" class="active">Visítenos</a>
-                    <a href="?controlador=Galery&accion=showGalery" class="active">Galería de fotos</a>
-<!--                    <a href="javascript:void(0);" class="icon" style="background-color: #B22222; color: black"
-                       onclick="updateClassName()">
-                        <i class="fa fa-bars" style="background-color: #B22222; color: black"></i>
-                    </a>-->
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                </div>
-
-                <script>
-                    function updateClassName() {
-                        var x = document.getElementById("myTopnav");
-                        if (x.className === "topnav") {
-                            x.className += " responsive";
-                        } else {
-                            x.className = "topnav";
-                        }
-                    }
-                </script>
-
+                
+                                <div class="topnav" id="myTopnav">
+                                    <a href="?controlador=Index&accion=mostrar" class="active">Inicio</a>
+                                    <a href="?controlador=History&accion=showHistory" class="active">Nuestra Historia</a>
+                                    <a href="?controlador=Products&accion=showProducts" class="active">Productos</a>
+                                    <a href="?controlador=Achievements&accion=showAchievements" class="active">Logros</a>
+                                    <a href="?controlador=Contact&accion=showContact" class="active">Contactos</a>
+                                    <a href="?controlador=VisitUs&accion=showVisitUs" class="active">Visítenos</a>
+                                    <a href="?controlador=Galery&accion=showGalery" class="active">Galería de fotos</a>
+                                    <a href="javascript:void(0);" class="icon" style="background-color: #B22222; color: black"
+                                       onclick="updateClassName()">
+                                        <i class="fa fa-bars" style="background-color: #B22222; color: black"></i>
+                                    </a>
+                                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                                        <span class="navbar-toggler-icon"></span>
+                                    </button>
+                                </div>
+                
+                                <script>
+                                    function updateClassName() {
+                                        var x = document.getElementById("myTopnav");
+                                        if (x.className === "topnav") {
+                                            x.className += " responsive";
+                                        } else {
+                                            x.className = "topnav";
+                                        }
+                                    }
+                                </script>
 
 
                 <!--                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                                    <a class="navbar-brand" href="#">Navbar</a>
-                                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                        <span class="navbar-toggler-icon"></span>
-                                    </button>
-                
+                                
                                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                                         <ul class="navbar-nav mr-auto">
                                             <li class="nav-item active">
-                                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                                                <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
                                             </li>
                                             <li class="nav-item">
-                                                <a class="nav-link" href="#">Link</a>
-                                            </li>
-                                            <li class="nav-item dropdown">
-                                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                    Dropdown
-                                                </a>
-                                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                                    <a class="dropdown-item" href="#">Action</a>
-                                                    <a class="dropdown-item" href="#">Another action</a>
-                                                    <div class="dropdown-divider"></div>
-                                                    <a class="dropdown-item" href="#">Something else here</a>
-                                                </div>
+                                                <a class="nav-link" href="#">Nuestra Historia</a>
+                                            </li>    
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="#">Productos</a>
+                                            </li> 
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="#">Logros</a>
                                             </li>
                                             <li class="nav-item">
-                                                <a class="nav-link disabled" href="#">Disabled</a>
+                                                <a class="nav-link" href="#">Contactos</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="#">Visítenos</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="#">Galería de fotos</a>
                                             </li>
                                         </ul>
-                                        <form class="form-inline my-2 my-lg-0">
-                                            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                                        </form>
+                                     
                                     </div>
                                 </nav>-->
 
 
+
+                
             </header>
 
             <section id="contenido">
