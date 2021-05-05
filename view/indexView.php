3ddba7e7b650847b35712772d18484634ed26e1b
Emily Melendez <emi.mci.95@gmail.com>
2021-05-05 16:02:21 -0600

    added div to description in visit us

--- view/indexView.php
+++ view/indexView.php
@@ -5,7 +5,7 @@ include_once 'public/header.php';
     <div class="row">
         <div class="col-sm">
             <main role="main" class="inner cover">
-                <h1 style="color: #B52222; margin-bottom: 5%;">Nuestra empresa</h1>
+                <h1 style="color: #B52222; margin-bottom: 2%;">Nuestra empresa</h1>
                 <h4 class="font-weight-bold" style="color:  #004063;">Lácteos Herencia es una empresa familiar de lácteos artesanales con su origen en El Torito, Santa Cruz de Turrialba, Costa Rica. Productores de leche a base del ganado Jersey, garantizando un producto de alta calidad y sabor</h4>
                 <a href="?controlador=Products&accion=showProducts" class="btn sample btn-lg active btn-sample">Ver Productos</a>
             </main>
