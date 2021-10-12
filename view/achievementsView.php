fde0848fb328b628193b29d3a29a0b02f56bb62e
Keilyn Ramirez <keilyn148@gmail.com>
2021-10-11 23:10:15 -0600

    Add information about achievements and add styles

--- view/achievementsView.php
+++ view/achievementsView.php
@@ -10,6 +10,18 @@ include_once 'public/headerSections.php';
             <img src="public/img/Vacas.jpg" id="imgAchievOne" alt="Queso Fresco" width="400" height="400">
         </div>
         <div class="col-12 col-sm-6 col-md-6 col-lg-6" id="achievementSection">
+            <h4 class="achievementsTitle">Certificado de nominación de Origen</h4>
+            <p>"La Denominación de Origen se da cuando el alimento es producido, transformado o elaborado en
+                un área geográfica delimitada, con un saber y conocimientos específicos reconocidos y
+                constatados. Se requiere un nexo entre el territorio, el producto y la tradición."
+            </p>
+            <p id="achievementsQuote">Dra. Patricia Sánchez Trejos | CICAP/UCR</p>
+        </div>
+    </div>
+
+    <div class="row text-center align-items-center">
+        <div class="col-12 col-sm-6 col-md-6 col-lg-6 text-center" id="achievementSection">
+            <h4 class="achievementsTitle">Galardón de la Bandera Azul Ecológica</h4>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
@@ -19,18 +31,8 @@ include_once 'public/headerSections.php';
                 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
         </div>
-    </div>
-
-    <div class="row text-center align-items-center">
-        <div class="col-12 col-sm-6 col-md-6 col-lg-6 text-center" id="achievementSection">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
-            </p>
-        </div>
         <div class="col-12 col-sm-6 col-md-6 col-lg-6" id="achievementSection">
-            <img src="public/img/galery7.jpg" id="imgAchievOne" alt="Queso Fresco" width="400" height=400">
+            <img src="public/img/BanderaAzul.png" id="imgAchievOne" alt="Queso Fresco" width="400" height="360">
         </div>
     </div>
 
